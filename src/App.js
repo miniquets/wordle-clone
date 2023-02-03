4b531afcbc5e2e4014662e57a29c6c8f9a22dfa2
Minique <minique@Jordans-MacBook-Pro.local>
2023-02-03 10:11:17 -0800

    added columns

--- src/App.js
+++ src/App.js
@@ -5,7 +5,56 @@ function App() {
   return (
     <div className="App">
       <div className="header">Wordle</div>
+
+      <div class="game-board">
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+      </div>
     </div>
   );
 }
+
 export default App;
