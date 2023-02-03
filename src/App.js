e68057b472f5a3d4ea91b5a5e7853017ad8ec4cc
Minique <minique@Jordans-MacBook-Pro.local>
2023-02-03 10:26:21 -0800

    added gameboard component

--- src/App.js
+++ src/App.js
@@ -1,58 +1,11 @@
-// import logo from "./logo.svg";
 import "./App.css";
+import Gameboard from "./Gameboard";
 
 function App() {
   return (
     <div className="App">
       <div className="header">Wordle</div>
-
-      <div class="game-board">
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-
-        <div className="row-module">
-          <div class="letter-box">2</div>
-          <div class="letter-box">3</div>
-          <div class="letter-box">4</div>
-          <div class="letter-box">5</div>
-          <div class="letter-box">5</div>
-        </div>
-      </div>
+      <Gameboard />
     </div>
   );
 }
---
+++ src/Gameboard.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+class Gameboard extends React.Component {
+  render() {
+    return (
+      <div class="game-board">
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+
+        <div className="row-module">
+          <div class="letter-box">2</div>
+          <div class="letter-box">3</div>
+          <div class="letter-box">4</div>
+          <div class="letter-box">5</div>
+          <div class="letter-box">5</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Gameboard;
